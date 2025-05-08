fdd3c8cbbeef71b8d98358fc82f780dfb8c03035
Florian Leca <florianleca@hotmail.fr>
2025-05-08 19:42:39 +0200

    initial commit

---
+++ src/main/java/com/flolecinc/inkvitebackend/InkviteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flolecinc.inkvitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkviteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InkviteBackendApplication.class, args);
+    }
+
+}
