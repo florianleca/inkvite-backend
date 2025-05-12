e4460ecfd49585ae66a9a041706f07b0a815e323
Florian Leca <florianleca@hotmail.fr>
2025-05-12 17:22:07 +0200

    #11 CORS configuration

---
+++ src/main/java/com/flolecinc/inkvitebackend/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.flolecinc.inkvitebackend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Value("${frontend.url}")
+    private String frontUrl;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(frontUrl)
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
